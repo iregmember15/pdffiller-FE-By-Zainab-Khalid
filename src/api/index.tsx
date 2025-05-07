c8841a62964c48b62cba680cc597dbe53349d61e
softwaredev786 <softwaredev786@gmail.com>
2025-05-07 16:08:39 +0500

    more changes for YML file success

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,7 +2,8 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
 
 // Set API base URL from environment variables
-const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+// const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+const API_BASE_URL = import.meta.env?.VITE_APP_BASE_URL || process.env.VITE_APP_BASE_URL || "";
 
 // Create Axios instance
 export const apiClient = axios.create({
